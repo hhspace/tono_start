98dd6337ae122ec029b19f084ccf35f60eaeff85
Maksim Golev <m.mudi@list.ru>
2017-08-20 12:25:34 +0300

    add edit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
